1d6cbbb8543be53c00af2f3cf41ec703762a42d2
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2013-03-10 20:22:19 +0100

    Renaming.

--- src/VisualBitcoin/Properties/AssemblyInfo.cs
+++ src/VisualBitcoin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VisualBitCoin")]
+[assembly: AssemblyTitle("VisualBitcoin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VisualBitCoin")]
+[assembly: AssemblyProduct("VisualBitcoin")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
