9245dec3df2fee91c4e08b0fdf959b894b18b9ee
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2013-03-13 20:33:42 +0100

    Renaming Data and Bitcoin.

--- src/Bitcoin/Bitcoin.cs
+++ src/Bitcoin/Bitcoin.cs
@@ -1,6 +1,12 @@
-﻿namespace Bitcoin
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitcoin
 {
-    public class Bitcoin
-    {
-    }
+	class Bitcoin
+	{
+	}
 }
--- src/Data/Data.cs
+++ src/Data/Data.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Data
 {
-    public class Data
-    {
-    }
+	class Data
+	{
+	}
 }
