15d8270d73ecbac53226d47a9837f9aafabfddb0
Nohemi Fernandez <nf68@cornell.edu>
2013-03-20 18:54:26 +0100

    Move RetrieveBlobsList() method to Storage

--- src/Storage/Storage.cs
+++ src/Storage/Storage.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
@@ -133,6 +135,34 @@ namespace Storage
         {
              // Retrieve reference to a previously created container.
             return blobClient.GetContainerReference(containerName);
+        }
+
+        public List<string> RetrieveBlobsList(CloudBlobClient blobClient, string containerName)
+        {
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer blobContainer = RetrieveContainer(blobClient, containerName);
+
+            var blobsList = new List<string>();
+
+            // Loop over blobs within the container and output the URI to each of them.
+            foreach (var blobItem in blobContainer.ListBlobs())
+            {
+                char[] separator = new char[] { '/' };
+                string blobUri = blobItem.Uri.ToString();
+                string[] splitedBlobUri = blobUri.Split(separator);
+                string blobName = splitedBlobUri[splitedBlobUri.Length - 1];
+                CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(blobName);
+                string text;
+                using (var memoryStream = new MemoryStream())
+                {
+                    blockBlob.DownloadToStream(memoryStream);
+                    text = System.Text.Encoding.UTF8.GetString(memoryStream.ToArray());
+
+                }
+                blobsList.Add(text);
+            }
+
+            return blobsList;
         }
 	}
 }
