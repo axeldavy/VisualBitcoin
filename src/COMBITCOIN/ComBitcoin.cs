a8389ce7183a9e7d1508f25fac1bb436c3e05269
Axel Davy <axel.davy@ens.fr>
2013-03-22 22:26:52 +0100

    Addition of Jayrock library
    Jayrock will be used to send request with bitcoin

---
+++ src/COMBITCOIN/ComBitcoin.cs
@@ -0,0 +1,42 @@
+﻿#region License, Terms and Conditions
+//
+// Jayrock - A JSON-RPC implementation for the Microsoft .NET Framework
+// Written by Atif Aziz (www.raboof.com)
+// Copyright (c) Atif Aziz. All rights reserved.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Net;
+using Jayrock.Json;
+using Jayrock.JsonRpc;
+
+namespace COMBITCOIN
+{
+    public class ComBitcoin
+    {
+        void ask_last_transactions(int number)
+        {
+            JsonRpcClient client = new JsonRpcClient();
+
+        }
+    }
+}
