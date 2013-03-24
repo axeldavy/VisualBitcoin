0f48c59d65543b1931f504d476a9f4ea450eaea1
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2013-03-24 02:58:53 +0100

    Creating a data model for blocks in table storage

---
+++ src/VisualBitcoin 1.1/Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Data
+{
+	class Context : Microsoft.WindowsAzure.StorageClient.TableServiceContext
+	{
+		public Context(string baseAddress, Microsoft.WindowsAzure.StorageCredentials credentials)
+			: base(baseAddress, credentials)
+		{
+
+		}
+
+		public IQueryable<Entry> Entry
+		{
+			get { return this.CreateQuery<Entry>("Entry"); }
+		}
+	}
+}
---
+++ src/VisualBitcoin 1.1/Data/Source.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace Data
+{
+	public class Source
+	{
+		// We define member fields for the data context and the storage account
+		// information.
+
+		// /!\ I don't know if readonly is required. (Baptiste)
+		private static readonly CloudStorageAccount StorageAccount;
+		private readonly Context _context;
+
+		// The static constructor initializes the storage account by reading its settings
+		// from the configuration and then create the tables used by the application from
+		// the model defined by the Context class
+		static Source()
+		{
+			StorageAccount = CloudStorageAccount.FromConfigurationSetting("");
+			CloudTableClient.CreateTablesFromModel(
+				typeof(Context),
+				StorageAccount.TableEndpoint.AbsoluteUri,
+				StorageAccount.Credentials);
+		}
+
+		// The public construstor initializes the data context used to access table
+		// storage.
+		public Source()
+		{
+			this._context = new Context(StorageAccount.TableEndpoint.AbsoluteUri, StorageAccount.Credentials)
+				{
+					RetryPolicy = RetryPolicies.Retry(3, TimeSpan.FromSeconds(1))
+				};
+		}
+
+		// This method returns the contents of Entry tables.
+		public IEnumerable<Entry> Entries()
+		{
+			// We retrieve today entries which is used by WebRole to bind a data grid and
+			// display the blocks.
+			var results =
+				from g in this._context.Entry
+				where g.PartitionKey == DateTime.UtcNow.ToString("YYYYMMDD")
+				select g;
+			return results;
+		}
+
+		// This method insert a new entry into the Entry table.
+		public void AddEntry(Entry entry)
+		{
+			this._context.AddObject("Entry", entry);
+			this._context.SaveChanges();
+		}
+	}
+}
