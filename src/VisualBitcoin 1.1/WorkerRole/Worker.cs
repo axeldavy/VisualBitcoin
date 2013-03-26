94a981297dc8a94583a5d9f14df4e5608817a9c9
Nohemi Fernandez <nf68@cornell.edu>
2013-03-26 01:21:50 +0100

    Add WorkerRole Bitcoin transaction method

---
+++ src/VisualBitcoin 1.1/WorkerRole/Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using Bitnet.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WorkerRole
+{
+    public class Worker
+    {
+        public void GetBitcoinTransactions() 
+        {
+            BitnetClient bc = new BitnetClient("http://127.0.0.1:8332");
+            bc.Credentials = new NetworkCredential("user", "pass");
+
+            var transactions = bc.InvokeMethod("listtransactions")["result"] as JArray;
+            Console.WriteLine(transactions);
+
+        }
+    }
+}
