72829af74467a2da22a5a4f421aa2ce045aee911
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2013-03-27 19:37:14 +0100

    Cleaning in the Worker Role of VisualBitcoin 1.1

--- src/VisualBitcoin 1.1/WorkerRole/Worker.cs
+++ src/VisualBitcoin 1.1/WorkerRole/Worker.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Net;
 using System.IO;
 using Bitnet.Client;
@@ -11,25 +8,27 @@ namespace WorkerRole
 {
     public class Worker
     {
-        private BitnetClient bitClient;
+        private readonly BitnetClient _bitClient;
 
         public Worker(String path)
         {
             // Read credentials file
-            StreamReader reader = new StreamReader(path);
+            var reader = new StreamReader(path);
             String credentials = reader.ReadToEnd();
-            string[] netInfo = credentials.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            string[] netInfo = credentials.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
             reader.Close();
 
             String user = netInfo[0];
             String password = netInfo[1];
-            this.bitClient = new BitnetClient("http://127.0.0.1:8332");
-            this.bitClient.Credentials = new NetworkCredential(user, password);
+            _bitClient = new BitnetClient("http://127.0.0.1:8332")
+	            {
+		            Credentials = new NetworkCredential(user, password)
+	            };
         }
 
         public JObject GetInfo()
         {
-            return this.bitClient.GetInfo();
+            return _bitClient.GetInfo();
         }
 
     }
