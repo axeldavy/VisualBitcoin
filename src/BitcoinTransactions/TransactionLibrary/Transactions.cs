a1d1df6dbb9d6a6cb4997a1ebbbadbac0c39b0de
Nohemi Fernandez <nf68@cornell.edu>
2013-03-27 20:08:22 +0100

    Outline Transaction methods

--- src/BitcoinTransactions/TransactionLibrary/Transactions.cs
+++ src/BitcoinTransactions/TransactionLibrary/Transactions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using Bitnet.Client;
 using Newtonsoft.Json.Linq;
 using System.Configuration;
@@ -10,12 +11,48 @@ namespace TransactionLibrary
 {
     public class Transactions
     {
-        public void GetTransactions() 
-        {
-            var configurationString = ConfigurationManager.AppSettings["bitcoinuser"];
+        BitnetClient bitClient;
+        JObject lastBlockStored;
 
+        public Transactions()
+        {
+            var user = ConfigurationManager.AppSettings["bitcoinuser"];
+            var password = ConfigurationManager.AppSettings["bitcoinpassword"];
+
+            this.bitClient = new BitnetClient("http://127.0.0.1:8332");
+            this.bitClient.Credentials = new NetworkCredential(user, password);
+
+            //TODO: get last block stored from Storage
+        }
+
+        public void PutBlocks(int max = 1000)
+        {
 
         }
 
+        public Boolean HasNewBlocks()
+        {
+            return false;
+        }
+
+        public void UploadNewBlocks(JArray arr) 
+        { 
+
+        }
+
+        public JObject GetTransaction(String txid) 
+        {
+            return null;
+        }
+
+        public JObject GetLastBlock()
+        {
+            return this.bitClient.InvokeMethod("listsinceblock")["result"] as JObject;
+        }
+
+        public JObject GetPreviousBlock(JObject obj)
+        {
+            return null;
+        }
     }
 }
