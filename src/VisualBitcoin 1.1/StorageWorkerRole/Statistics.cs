11f412b8b1dd87fce498a13d1e3b093daac574ab
BaptisteLefebvre <baptiste.lefebvre@ens.fr>
2013-05-03 17:55:36 +0200

    Retrieve Build, instability of the Explorer

--- src/VisualBitcoin 1.1/StorageWorkerRole/Statistics.cs
+++ src/VisualBitcoin 1.1/StorageWorkerRole/Statistics.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Globalization;
+using System.Collections.Generic;
 using Storage;
 using Storage.Models;
 
@@ -8,32 +8,32 @@ namespace StorageWorkerRole
 	class Statistics
 	{
 		// Statistics Variable :
-        static ulong _numberOfBlocks; // Number_of_Blocks
-        
-        // Time.
-        static ulong _totalTime;
-        static double _averageTime;
-        static double _varianceTime;
+		static ulong _numberOfBlocks; // Number_of_Blocks
+
+		// Time.
+		static ulong _totalTime;
+		static double _averageTime;
+		static double _varianceTime;
 		// ReSharper disable NotAccessedField.Local
-        static double _standardDeviationTime;
+		static double _standardDeviationTime;
 		// Number of seconds between 3 January 2009 (first BTC ?) and 1 January 1970.
-        private const ulong InitialTime = 1230940800;
+		private const ulong InitialTime = 1230940800;
 
-        // Statistics blocks BTC.
-        private static ulong _sommeBtc;
-        static double _averageBtc;
-        static double _standardDeviationBtc;
-        static double _varianceBtc;
+		// Statistics blocks BTC.
+		private static ulong _sommeBtc;
+		static double _averageBtc;
+		static double _standardDeviationBtc;
+		static double _varianceBtc;
 		// Pourcentage bloc invalide ??
-        //static double Invalide_block;
+		//static double Invalide_block;
 
-        // Statistics blocks transactions.
-        static ulong _numberOfTransactions;
-        static double _averageT;
-        static double _varianceT;
-        static double _standardDeviationT;
+		// Statistics blocks transactions.
+		static ulong _numberOfTransactions;
+		static double _averageT;
+		static double _varianceT;
+		static double _standardDeviationT;
 
-        // Statistics transactions.
+		// Statistics transactions.
 		/* // Number_of_transactions
 		static ulong Number_of_Transactions;
 		static double Standard_deviation_T;
@@ -41,124 +41,120 @@ namespace StorageWorkerRole
 
 
 		// Retrieve a block from queue and call Review_Statics.
-        // Added parsing here
-// ReSharper disable UnusedMember.Local
-        static void Main()
-        {
-            var hash = Queue.PopMessage<string>();
-            var block = Blob.GetBlock(hash);
+		// Added parsing here
+		// ReSharper disable UnusedMember.Local
+		static void Main()
+		{
+			var hash = Queue.PopMessage<string>();
+			var block = Blob.GetBlock(hash);
+
+			Review_Statistics(block);
+			Sort_blocks(block);
+
+			/*
+			 * Can't add this because I can't test it(((
+			//Blob.DeleteBlockBlob(hash);
             
-            Review_Statistics(block);
-            Sort_blocks(block);
+			//Sending clear block to the Blob storage
+			Blob.UploadBlockBlob<BlockClear>(clearBlock.Hash, clearBlock);
+			//Sending transactions to the Blob storage
+			foreach (Transactions singleTransaction in parsed.Item2)
+			{
+				Blob.UploadBlockBlob<Transactions>(singleTransaction.Txid, singleTransaction);
+			}*/
+		}
 
-            /*
-             * Can't add this because I can't test it(((
-            //Blob.DeleteBlockBlob(hash);
-            
-            //Sending clear block to the Blob storage
-            Blob.UploadBlockBlob<BlockClear>(clearBlock.Hash, clearBlock);
-            //Sending transactions to the Blob storage
-            foreach (Transactions singleTransaction in parsed.Item2)
-            {
-                Blob.UploadBlockBlob<Transactions>(singleTransaction.Txid, singleTransaction);
-            }*/
-        }
+		//List<Block> Lastblocks = new List<Block>();
 
-        //List<Block> Lastblocks = new List<Block>();
+		private static int Compare_block(Block x, Block y)
+		{
+			if (x == null)
+				return -1;
 
-        private static int Compare_block(Block x,Block y)
-        {
-            if (x == null)
-            {
-                return -1;
-            }
-            else
-            {
-                if (y == null)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return (x.Time >= y.Time);
-                }
-            }
-        }
-        //TODO: initialiser à 10 null
-        private static void Sort_blocks(Block block)
-        {
-            var blocklist = DownloadBlockBlob<(List<Block>)>("Last_Blocks");
-            blocklist.Add(block);
-            blocklist.Sort(Compare_block);
-            blocklist.RemoveAt(0);
-            UploadBlockBlob("Last_Blocks", (List<Block>) blocklist);
-        }
+			if (y == null)
+				return 1;
 
-        //TODO : à déplacer et initialiser les Higher_Block_Sort_
-	    /*private static void Sort_high_blocks(Block block)
-	    {
-	        int n = 9; // number of blocks - 1
-            var blockinarray = Blob.GetBlockHigh("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture));
-            var blockhigh = new BlockHigh(block.Hash, block.Time);
+			if (x.Time <= y.Time)
+				return -1;
+
+			return 1;
+		}
+
+		//TODO: initialiser à 10 null
+		private static void Sort_blocks(Block block)
+		{
+			var blocklist = Blob.DownloadBlockBlob<List<Block>>("Last_Blocks");
+			blocklist.Add(block);
+			blocklist.Sort(Compare_block);
+			blocklist.RemoveAt(0);
+			Blob.UploadBlockBlob("Last_Blocks", (List<Block>)blocklist);
+		}
+
+		//TODO : à déplacer et initialiser les Higher_Block_Sort_
+		/*private static void Sort_high_blocks(Block block)
+		{
+			int n = 9; // number of blocks - 1
+			var blockinarray = Blob.GetBlockHigh("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture));
+			var blockhigh = new BlockHigh(block.Hash, block.Time);
             
 
-            while ((n >= 0) && (block.Time) < (blockinarray.Time))
-            {
-                blockinarray = Blob.GetBlockHigh("Higher_Block_Sort_"+ n.ToString(CultureInfo.InvariantCulture));// Attention sur le Hash
-                n--;
-            }
-	        if (n >= 0)//simplifier
-	        {
-                var previousblockhigh = Blob.GetBlockHigh("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture));
+			while ((n >= 0) && (block.Time) < (blockinarray.Time))
+			{
+				blockinarray = Blob.GetBlockHigh("Higher_Block_Sort_"+ n.ToString(CultureInfo.InvariantCulture));// Attention sur le Hash
+				n--;
+			}
+			if (n >= 0)//simplifier
+			{
+				var previousblockhigh = Blob.GetBlockHigh("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture));
 // ReSharper disable RedundantAssignment
-                var permuteblockhigh = new BlockHigh();
+				var permuteblockhigh = new BlockHigh();
 // ReSharper restore RedundantAssignment
-                Blob.UploadBlockBlob("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture), blockhigh);
-	            n--;
-                while (n >= 0)
-                {
-                    permuteblockhigh = Blob.GetBlockHigh("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture));
-                    Blob.UploadBlockBlob("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture), previousblockhigh);
-                    previousblockhigh = permuteblockhigh;
-                    n--;
-                }
-	        }
+				Blob.UploadBlockBlob("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture), blockhigh);
+				n--;
+				while (n >= 0)
+				{
+					permuteblockhigh = Blob.GetBlockHigh("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture));
+					Blob.UploadBlockBlob("Higher_Block_Sort_" + n.ToString(CultureInfo.InvariantCulture), previousblockhigh);
+					previousblockhigh = permuteblockhigh;
+					n--;
+				}
+			}
 
-	    }*/
+		}*/
 
-	    // Update statistics.
-        static void Review_Statistics(Block x)
-        {
-            _numberOfBlocks += 1;
-            _numberOfTransactions += Convert.ToUInt64(x.NumberOfTransactions); 
+		// Update statistics.
+		static void Review_Statistics(Block x)
+		{
+			_numberOfBlocks += 1;
+			_numberOfTransactions += Convert.ToUInt64(x.NumberOfTransactions);
 
-            // Qualite de temps de distribution des blocs.
-            _totalTime = (_totalTime + Convert.ToUInt64(x.Time) - InitialTime);
+			// Qualite de temps de distribution des blocs.
+			_totalTime = (_totalTime + Convert.ToUInt64(x.Time) - InitialTime);
 			// ReSharper disable PossibleLossOfFraction
-            _averageTime = _totalTime / _numberOfBlocks;
-            _varianceTime = Variance(_totalTime, _averageTime, _numberOfBlocks);
-            _standardDeviationTime = Math.Sqrt(_varianceTime);
-            
+			_averageTime = _totalTime / _numberOfBlocks;
+			_varianceTime = Variance(_totalTime, _averageTime, _numberOfBlocks);
+			_standardDeviationTime = Math.Sqrt(_varianceTime);
 
-            // Statistics blocks (BTC).
+
+			// Statistics blocks (BTC).
 			// Changer avec BTC
 			_sommeBtc += Convert.ToUInt64(x.Size);
 			_averageBtc = _sommeBtc / _numberOfBlocks;
-            _varianceBtc = Variance(_sommeBtc, _averageBtc, _numberOfBlocks);
-            _standardDeviationBtc = Math.Sqrt(_varianceBtc);
+			_varianceBtc = Variance(_sommeBtc, _averageBtc, _numberOfBlocks);
+			_standardDeviationBtc = Math.Sqrt(_varianceBtc);
 
 
-            // Statistics blocks (Transactions).
-            _numberOfTransactions += Convert.ToUInt64(x.NumberOfTransactions);
-            _averageT = _numberOfTransactions / _numberOfBlocks;
-            _varianceT = Variance(_numberOfTransactions, _averageT, _numberOfBlocks);
-            _standardDeviationT = Math.Sqrt(_varianceT);
-        }
+			// Statistics blocks (Transactions).
+			_numberOfTransactions += Convert.ToUInt64(x.NumberOfTransactions);
+			_averageT = _numberOfTransactions / _numberOfBlocks;
+			_varianceT = Variance(_numberOfTransactions, _averageT, _numberOfBlocks);
+			_standardDeviationT = Math.Sqrt(_varianceT);
+		}
 
-        static double Variance(ulong somme,double average, ulong nb)
-        {
-            return somme*(somme - 2*average)/nb + average*average;
-        }
+		static double Variance(ulong somme, double average, ulong nb)
+		{
+			return somme * (somme - 2 * average) / nb + average * average;
+		}
 
 	}
 }
