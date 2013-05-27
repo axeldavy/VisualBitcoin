a3866c28693e3cbb558c4a469a3602dfc7a5d316
Ken Chanseau Saint-Germain <csgk@me.com>
2013-05-27 15:54:44 +0200

    Add a complete interface for statistics

---
+++ src/VisualBitcoin 1.1/WebRole/Controllers/StatisticsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Storage.Models;
+using Storage;
+
+namespace WebRole.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        //
+        // GET: /Statistics/
+        private Blob blob;
+
+        public ActionResult Index()
+        {
+            var connectionString = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            blob = new Blob(storageAccount);
+
+            Statistics stat = (blob.GetStatistics<Statistics>("General_Statistics"));
+
+            return View(stat);
+        }
+
+    }
+}
---
+++ src/VisualBitcoin 1.1/WebRole/Models/StatisticsModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Storage.Models;
+
+namespace WebRole.Models
+{
+    public class StatisticsModel 
+    {
+        //
+        [Display(Name = "Number of Blocks")]
+        public ulong NumberOfBlocks { get; set; }
+        // Time.
+        [Display(Name = "à compléter")]
+        public ulong TotalTime { get; set; }
+        [Display(Name = "à compléter")]
+        public double AverageTime { get; set; }
+        [Display(Name = "à compléter")]
+        public double VarianceTime { get; set; }
+        [Display(Name = "à compléter")]
+        public double StandardDeviationTime { get; set; }
+
+        // Statistics blocks BTC.
+        [Display(Name = "à compléter")]
+        public ulong SumBtc { get; set; }
+        [Display(Name = "à compléter")]
+        public double AverageBtc { get; set; }
+        [Display(Name = "à compléter")]
+        public double StandardDeviationBtc { get; set; }
+        [Display(Name = "à compléter")]
+        public double VarianceBtc { get; set; }
+
+        // Statistics blocks transactions.
+        [Display(Name = "à compléter")]
+        public ulong NumberOfTransactions { get; set; }
+        [Display(Name = "à compléter")]
+        public double AverageTrans { get; set; }
+        [Display(Name = "à compléter")]
+        public double VarianceTrans { get; set; }
+        [Display(Name = "à compléter")]
+        public double StandardDevTrans { get; set; }
+
+        public StatisticsModel(Statistics stats)
+        {
+            TotalTime = stats.TotalTime;
+            AverageTime = stats.AverageTime;
+            VarianceTime = stats.VarianceTime;
+            StandardDeviationTime = stats.StandardDeviationTime;
+            SumBtc = stats.SumBtc;
+            AverageBtc = stats.AverageBtc;
+            StandardDeviationBtc = stats.StandardDeviationBtc;
+            VarianceBtc = stats.VarianceBtc;
+            NumberOfTransactions = stats.NumberOfTransactions;
+            AverageTrans = stats.AverageTrans;
+            VarianceTrans = stats.VarianceTrans;
+            StandardDevTrans = stats.StandardDevTrans;
+        }
+
+    }
+}
