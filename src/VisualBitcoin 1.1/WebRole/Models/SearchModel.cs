22ee91cf7c9f9a62e7727ace820286b47b69a582
Ken Chanseau Saint-Germain <csgk@me.com>
2013-05-27 16:41:04 +0200

    Add new pages in the menu

--- src/VisualBitcoin 1.1/WebRole/Models/SearchModel.cs
+++ src/VisualBitcoin 1.1/WebRole/Models/SearchModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using Storage;
@@ -8,6 +9,7 @@ namespace WebRole.Models
 {
     public class SearchModel
     {
+        [Required(ErrorMessage = "Champ vide !")]
         public string Name { get; set; }
     }
 }
